f5f18d96f65aed57cab8f6a18079437f642a819c
Daniel Cornelius <dcorn942@gmail.com>
2013-08-14 18:34:47 -0500

    minor changes
    +Added nGUI (previous commit, forgot to add to list of additions)
    +Added proper player controllers
    -Removed unneeded scripts
    +Added very basic map + character

--- Assets/Core/C#/MainCameraFOV.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class KSGameManager : MonoBehaviour {
-	//public float _FOV = 90.0f;
-    void LateUpdate() {
-       // Camera.main.fieldOfView = _FOV;
-    }
-}
